6c9ebedef645e715aede5e61d2f4687534aa9dcc
Dasha <dashakonopelko@gmail.com>
2025-09-05 16:49:47 +0300

    cycles

---
+++ 02_operators_helper_home/02_operators_helper_home.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*Завдання 3
+		Користувач вводить із клавіатури 7 цілих чисел.Напишіть програму, яка визначає максимальне з цих 7 чисел(Підказка:  розв'язок має бути простим).*/
+
+	/*int numb_1, numb_2, numb_3, numb_4, numb_5, numb_6, numb_7;
+
+	cout << "Enter numbers :: ";
+	cin >> numb_1 >> numb_2 >> numb_3 >> numb_4 >> numb_5 >> numb_6 >> numb_7;
+
+	int max = numb_1;
+	if (max < numb_2)
+		max = numb_2;
+	if (max < numb_3)
+		max = numb_3;
+	if (max < numb_4)
+		max = numb_4;
+	if (max < numb_5)
+		max = numb_5;
+	if (max < numb_6)
+		max = numb_6;
+	if (max < numb_7)
+		max = numb_7;*/
+	/*int number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	int max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Enter numbers :: ";
+	cin >> number;
+	if (max < number)
+		max = number;
+	cout << "Max number :: " << max << endl;*/
+
+
+	/*ористувач вводить з клавіатури ціле шестизначне число.Написати програму, яка визначає, чи є введене число - щасливим(Щасливим вважається шестизначне число, у якого сума перших 3 цифр дорівнює сумі других трьох цифр).Якщо користувач ввів не шестизначне число - повідомлення про помилку.*/
+
+	//int number = 123456;
+	//int a, b, c, d, e, f;
+
+	//f = number % 10; // 6
+	//number = number / 10; // number(123456) / 10 = 12345.6 --> int 12345
+
+	//e = number % 10; // 5
+	//number = number / 10; // number(12345) / 10 = 1234.5 --> int 1234
+
+	//d = number % 10; // 4
+	//number = number / 10; // number(1234) / 10 = 123.4 --> int 123
+
+	//c = number % 10; // 3
+	//number = number / 10; // number(123) / 10 = 12.3 --> int 12
+
+	//b = number % 10; // 2
+	//number = number / 10; // number(12) / 10 = 1.2 --> int 1
+
+	//a = number % 10; // 1
+	//number = number / 10; // number(1) / 10 = 0.1 --> int 0
+}
---
+++ 03_cycles/03_cycles.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+enum colors {
+	GREEN = 1, RED, YELLOW, AQUA, PURPLE
+};
+
+int main()
+{
+	/*
+		while(){
+		}
+		for(){}
+		do{} while()
+	*/
+
+	/*int i = 0;
+	while (i < 10)
+	{
+		i++;
+		cout << i << endl;
+	}*/
+
+	/*int res;
+	do
+	{
+		cout << "2 + 2 = ";
+		cin >> res;
+	} while (res != 4);
+
+	cout << "Congratulation" << endl;*/
+	// range(start, end, step);
+	// range(10)
+
+	/*size_t i = 0;
+	for (; ; )
+	{
+		i++;
+		cout << i << "\t";
+		if (i == 10)
+			break;
+	}
+	cout << i << endl;*/
+	//for (size_t i = 1; i <= 20; i++)
+	//{
+	//	if (i % 3 == 0)
+	//		continue;
+
+	//	cout << i << "\t";
+	//}
+	//cout << endl;
+
+
+	int choice;
+
+	do
+	{
+		system("cls");
+		cout << R"(
+		1 - Green;
+		2 - Red;
+		3 - Yellow;
+		4 - Aqua;
+		5 - Purple;
+		0 - Exit;
+		Enter choice :: )";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case colors::GREEN:
+			system("color 02"); break;
+		case colors::RED:
+			system("color 04"); break;
+		case colors::YELLOW:
+			system("color 06"); break;
+		case colors::AQUA:
+			system("color 03"); break;
+		case colors::PURPLE:
+			system("color 05"); break;
+		}
+	} while (choice != 0);
+	
+
+
+}
+
+
