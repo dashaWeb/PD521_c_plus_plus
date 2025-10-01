81f97916184fc50f6c09cfe02b80ffeb882a1315
Dasha <dashakonopelko@gmail.com>
2025-10-01 17:40:44 +0300

    multi array

---
+++ 14_с_іекштп_рудзук_рщьу/source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void task6(char* str)
+{
+	int counter_letter = 0;
+	int counter_digit = 0;
+	int counter_other = 0;
+
+	for (size_t i = 0; /*i < strlen(str)*/ str[i] != '\0'; i++)
+	{
+		if (isalpha(str[i])) counter_letter++;
+		else if (isdigit(str[i])) counter_digit++;
+		else counter_other++;
+	}
+	cout << "Number of letters :: " << counter_letter << endl;
+	cout << "Number of digits  :: " << counter_digit << endl;
+	cout << "Number of other   :: " << counter_other << endl;
+}
+void task4(char* str)
+{
+	for (size_t i = 0; /*i < strlen(str)*/ str[i] != '\0'; i++)
+	{
+		if (str[i] == '.') str[i] = '!';
+		
+	}
+}
+int main()
+{
+   /* Користувач вводить рядок.Визначити кількість літер, кількість цифр і кількість інших символів, присутніх у рядку.*/
+	//Lorem ipsum dolor sit amet, 2consectetur adipiscing elit. Curabitur nec aliquet turpis, nec luctus neque. Quisque ante odio, commodo ut aliquet vel, imperdiet3 vel augue. Vestibulum at3 nisl non ante maximus sodales ac in nulla. Cras erat ligula, cursus ut sem at, vehicula ultric5es nulla. Mauris posuer0e suscipit erat, at consequat dolor egestas sed. Quisque accumsan condimentum luctus. Pellentesque sit am2et felis nec lacus scelerisque placerat ac eu massa. 
+	char line[1000];
+	cout << "enter line :: ";
+	cin.getline(line, 1000);
+	task6(line);
+	task4(line);
+	cout << "Result :: " << line << endl;
+
+}
---
+++ 15_multi array/15_multi array.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+template <typename T>
+void fill(T** arr, const int& row, const int& col, const int& min = 1, const int& max = 10)
+{
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < col; j++)
+		{
+			arr[i][j] = min + rand() % (max - min + 1);
+		}
+	}
+}
+
+template <typename T>
+void print(T** arr, const int& row, const int& col, const string prompt = "")
+{
+	cout << (prompt.empty() ? "" : prompt + " :: \n");
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+//Написати функцію, що додає рядок двовимірному масиву в кінець.
+template<typename T>
+void pushBack(T**& arr, int& row, const int& col) {
+
+	T** tmp = new T * [row + 1];
+	for (size_t i = 0; i < row; i++)
+	{
+		tmp[i] = arr[i];
+	}
+	tmp[row] = new T[col]{};
+	row++;
+	delete[] arr;
+	arr = tmp;
+}
+//Написати функцію, що додає рядок двовимірному масиву на початок.
+template<typename T>
+void pushFront(T**& arr, int& row, const int& col) {
+
+}
+//Написати функцію, що додає рядок двовимірному масиву в зазначену позицію.
+template<typename T>
+void insert(T**& arr, int& row, const int& col, const int& index) {
+	if (index < 0 or index > row) {
+		cout << "Error Index" << endl;
+		return;
+	}
+	T** tmp = new T * [row + 1];
+	for (size_t i = 0; i < row; i++)
+	{
+		if (i < index)
+		{
+			tmp[i] = arr[i];
+		}
+		else {
+			tmp[i + 1] = arr[i];
+		}
+	}
+	tmp[index] = new T[col]{};
+	row++;
+	delete[] arr;
+	arr = tmp;
+}
+//Написати функцію, що видаляє рядок двовимірного масиву за вказаним номером.
+template<typename T>
+void remove(T**& arr, int& row, const int& index) {
+	if (index < 0 or index >= row) {
+		cout << "Error Index" << endl;
+		return;
+	}
+	T** tmp = new T * [--row];
+	for (size_t i = 0; i < row; i++)
+	{
+		if (i < index)
+		{
+			tmp[i] = arr[i];
+		}
+		else {
+			tmp[i] = arr[i + 1];
+		}
+	}
+	delete[] arr[index];
+	delete[] arr;
+	arr = tmp;
+}
+int main()
+{
+	int row = 4, col = 6;
+	cout << "Enter size :: ";
+	//cin >> row >> col;
+	int** arr = new int* [row];
+
+
+	for (size_t i = 0; i < row; i++)
+	{
+		arr[i] = new int[col];
+	}
+
+	fill(arr, row, col);
+	print(arr, row, col, "Print Mult Array");
+
+	cout << endl;
+
+	pushBack(arr, row, col);
+	print(arr, row, col, "Print Push Back ");
+
+	cout << endl;
+
+	insert(arr, row, col, 2);
+	print(arr, row, col, "Print insert   ");
+
+
+	cout << endl;
+	remove(arr, row, 3);
+	print(arr, row, col, "Print insert   ");
+
+
+	for (size_t i = 0; i < row; i++)
+	{
+		delete[] arr[i];
+	}
+
+	delete[] arr;
+
+}
