a27ee9b85e53efa6310172b4590f2a5858fdb0a5
Dasha <dashakonopelko@gmail.com>
2025-10-03 15:36:05 +0300

    helper home

--- 15_multi array/15_multi array.cpp
+++ 15_multi array/15_multi array.cpp
@@ -43,6 +43,8 @@ void pushBack(T**& arr, int& row, const int& col) {
 	arr = tmp;
 }
 //Написати функцію, що додає рядок двовимірному масиву на початок.
+//Написати функцію, що видаляє рядок двовимірного масиву на початку.
+//Написати функцію, що видаляє рядок двовимірного масиву в кінці.
 template<typename T>
 void pushFront(T**& arr, int& row, const int& col) {
 
@@ -92,6 +94,77 @@ void remove(T**& arr, int& row, const int& index) {
 	delete[] arr;
 	arr = tmp;
 }
+
+template <typename T>
+void deleteArray(T*& arr)
+{
+	delete[] arr;
+	arr = nullptr;
+}
+
+template <typename T>
+void insert(T*& arr, int& size, T value, int index)
+{
+	if (index < 0 or index > size)
+	{
+		cout << "Error Index"; return;
+	}
+	T* tmp = new T[size + 1];
+	tmp[index] = value;
+	for (size_t i = 0; i < size; i++)
+	{
+		if (i < index)
+			tmp[i] = arr[i];
+		else
+			tmp[i + 1] = arr[i];
+	}
+	size++;
+	deleteArray(arr);
+	arr = tmp;
+
+}
+template<typename T>
+void remove(T*& arr, int& size, int index)
+{
+	if (index < 0 or index >= size)
+	{
+		cout << "Error Index"; return;
+	}
+	T* tmp = new T[--size];
+	for (size_t i = 0; i < size; i++)
+	{
+		if (i < index)
+			tmp[i] = arr[i];
+		else
+			tmp[i] = arr[i + 1];
+	}
+	deleteArray(arr);
+	arr = tmp;
+}
+
+
+template<typename T>
+void insertColumn(T** arr, const int& row, int& col, const int& index, T value = T())
+{
+	int col_ = col;
+	for (size_t i = 0; i < row; i++)
+	{
+		insert(arr[i], col_, value, index);
+	}
+	col++;
+}
+
+template<typename T>
+void removeColumn(T** arr, const int& row, int& col, const int& index)
+{
+	for (size_t i = 0; i < row; i++)
+	{
+		int col_ = col;
+		remove(arr[i], col_, index);
+	}
+	col--;
+}
+
 int main()
 {
 	int row = 4, col = 6;
@@ -123,6 +196,15 @@ int main()
 	remove(arr, row, 3);
 	print(arr, row, col, "Print insert   ");
 
+
+	cout << endl;
+	insertColumn(arr, row, col, 2, 0);
+	print(arr, row, col, "Print insertColumn   ");
+
+
+	cout << endl;
+	removeColumn(arr, row, col, 3);
+	print(arr, row, col, "Print removetColumn   ");
 
 	for (size_t i = 0; i < row; i++)
 	{
